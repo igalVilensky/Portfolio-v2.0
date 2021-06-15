6e3a9d127d6725737d5ad22828c56f631037987c
igalv <vilenskyigal@gmail.com>
2021-06-15 15:34:44 +0200

    top section and scrol

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "./topbar.scss";
+import { Person, Mail, LocalPhone } from "@material-ui/icons";
 
 function Topbar() {
   return (
-    <div className="topbar" id="topbar">
+    <div className="topbar">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
             Igal Vilensky ©
           </a>
+          <div className="itemContainer">
+            <LocalPhone className="icon" />
+            <span>+49 458 22 54</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>vilenskyigal@gmail.com</span>
+          </div>
         </div>
         <div className="right"></div>
       </div>
