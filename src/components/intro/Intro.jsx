113a1ce2e6cdc4126900044e2b4b7e6b33d028fd
igalv <vilenskyigal@gmail.com>
2021-06-16 16:21:30 +0200

    image fix

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./intro.scss";
-/* import Photo from "ddd.jpeg"; */
+
 function Intro() {
   return (
     <div className="intro" id="intro">
       <div className="left">
         <div className="img__container">
-          {/*  <img src={Photo} alt="me" /> */}
+          <img src={`${process.env.PUBLIC_URL}/assets/me.png`} alt="me" />
         </div>
       </div>
       <div className="right"></div>
