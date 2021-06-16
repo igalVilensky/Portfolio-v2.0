f4a6a21dc2d0de092f895352981232ba09cdcf3a
igalv <vilenskyigal@gmail.com>
2021-06-16 15:44:30 +0200

    intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./intro.scss";
+/* import Photo from "ddd.jpeg"; */
 function Intro() {
   return (
     <div className="intro" id="intro">
-      intro
+      <div className="left">
+        <div className="img__container">
+          {/*  <img src={Photo} alt="me" /> */}
+        </div>
+      </div>
+      <div className="right"></div>
     </div>
   );
 }
