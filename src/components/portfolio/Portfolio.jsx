ba2250812fb5cf99a654a7567893fa7993bbfd63
igalv <vilenskyigal@gmail.com>
2021-06-17 14:37:58 +0200

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,20 @@ import "./portfolio.scss";
 function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Branding</li>
+        <li>Design</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt=""></img>
+          <h3>Forex App</h3>
+        </div>
+      </div>
     </div>
   );
 }
