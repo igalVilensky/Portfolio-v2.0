add0ba5f4768aaa179cf8a4c16d38be4078a7f38
igalv <vilenskyigal@gmail.com>
2021-06-17 11:14:36 +0200

    heading fix

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -25,7 +25,7 @@ function Intro() {
           <h2>Hi there, I'm </h2>
           <h1>Igal Vilensky</h1>
           <h3>
-            first-class <span ref={textRef}></span>
+            Leading <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
