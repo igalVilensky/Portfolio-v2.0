b78bcda0b18c5b5135da45a0d5a844f68ba10a26
igalv <vilenskyigal@gmail.com>
2021-06-19 14:05:23 +0200

    portfolio data, useEffect

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./portfolio.scss";
 import PortfolioList from "../portfolioList/PortfolioList";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data.js";
+
 function Portfolio() {
   const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
 
   const list = [
     { id: "featured", title: "Featured" },
@@ -11,6 +20,28 @@ function Portfolio() {
     { id: "content", title: "Content" },
     { id: "design", title: "Design" },
   ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(contentPortfolio);
+    }
+  }, [selected]);
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
@@ -25,48 +56,13 @@ function Portfolio() {
         ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/portfolio.png`}
-            alt=""
-          ></img>
-          <h3>Portfolio website</h3>
-        </div>
-        <div className="item">
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/portfolio2.png`}
-            alt=""
-          ></img>
-          <h3>Bootstrap website</h3>
-        </div>
-        <div className="item">
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/portfolio.png`}
-            alt=""
-          ></img>
-          <h3>Portfolio website</h3>
-        </div>
-        <div className="item">
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/portfolio2.png`}
-            alt=""
-          ></img>
-          <h3>Bootstrap website</h3>
-        </div>
-        <div className="item">
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/portfolio.png`}
-            alt=""
-          ></img>
-          <h3>Portfolio website</h3>
-        </div>
-        <div className="item">
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/portfolio.png`}
-            alt=""
-          ></img>
-          <h3>Portfolio website</h3>
-        </div>
+        {" "}
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt={d.title}></img>
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
