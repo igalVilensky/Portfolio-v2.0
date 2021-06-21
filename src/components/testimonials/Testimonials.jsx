d330ec3e2bab0ea54b413466165f029059039995
igalv <vilenskyigal@gmail.com>
2021-06-21 14:07:31 +0200

    destruction, featured, img issue fix

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,37 +1,55 @@
 import React from "react";
 import "./testimonials.scss";
 export default function Testimonials() {
+  const data = [
+    {
+      id: 1,
+      name: "Vlad Sol",
+      title: "Senior Developer at Cashdo Ltd.",
+      img: `${process.env.PUBLIC_URL}/assets/senDev.jpeg`,
+      icon: `${process.env.PUBLIC_URL}/assets/twitter.png`,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+    },
+    {
+      id: 2,
+      name: "Alex Muller",
+      title: "Co-Founder of MyFxBook",
+      img: `${process.env.PUBLIC_URL}/assets/ceo.jpg`,
+      icon: `${process.env.PUBLIC_URL}/assets/youtube.png`,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Pavel Rekun",
+      title: "CEO of PaybackFx",
+      img: `${process.env.PUBLIC_URL}/assets/ceo2.jpg`,
+      icon: `${process.env.PUBLIC_URL}/assets/linkedin.png`,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
   return (
     <div className="testimonials" id="testimonials">
       <h1>Testimonials</h1>
       <div className="container">
-        <div className="card">
-          <div className="top">
-            <img
-              src={`${process.env.PUBLIC_URL}/assets/rightArrow.png`}
-              alt="rightArrow"
-              className="left"
-            />
-            <img
-              src={`${process.env.PUBLIC_URL}/assets/senDev.jpeg`}
-              alt="rightArrow"
-              className="user"
-            />
-            <img
-              src={`${process.env.PUBLIC_URL}/assets/youtube.png`}
-              alt="rightArrow"
-              className="right"
-            />
+        {data.map((d) => (
+          <div className={d.featured ? "card featured" : "card"}>
+            <div className="top">
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/rightArrow.png`}
+                alt="rightArrow"
+                className="left"
+              />
+              <img src={d.img} alt="rightArrow" className="user" />
+              <img src={d.icon} alt="rightArrow" className="right" />
+            </div>
+            <div className="center">{d.desc}</div>
+            <div className="bottom">
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
           </div>
-          <div className="center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-            impedit sed reprehenderit quo, iure magni molestiae dolor
-          </div>
-          <div className="bottom">
-            <h3>David</h3>
-            <h4>CEO of Cashdo Ltd.</h4>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
