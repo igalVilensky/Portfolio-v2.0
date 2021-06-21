1d641e122905cef3ed696c14c6598b916ba4a7fc
igalv <vilenskyigal@gmail.com>
2021-06-21 16:33:01 +0200

    contact form and svg

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,26 @@ import "./contact.scss";
 function Contact() {
   return (
     <div className="contact" id="contact">
-      contact
+      <div className="left">
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/contact.svg`}
+          alt="contact"
+        />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form>
+          <input type="text" placeholder="eMail" />
+          <textarea
+            placeholder="Message"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   );
 }
