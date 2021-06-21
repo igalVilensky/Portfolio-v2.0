d74a9504e477bfc2369935f21416bd76dd56b0db
igalv <vilenskyigal@gmail.com>
2021-06-21 12:02:50 +0200

    testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,14 @@ import "./testimonials.scss";
 export default function Testimonials() {
   return (
     <div className="testimonials" id="testimonials">
-      testimonials
+      <h1>Testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top"></div>
+          <div className="center"></div>
+          <div className="bottom"></div>
+        </div>
+      </div>
     </div>
   );
 }
