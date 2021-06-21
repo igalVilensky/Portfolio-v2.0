a41ecc775c159332c5fe298f45edf44147c99a9f
igalv <vilenskyigal@gmail.com>
2021-06-21 12:46:53 +0200

    cards and styling

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -6,9 +6,31 @@ export default function Testimonials() {
       <h1>Testimonials</h1>
       <div className="container">
         <div className="card">
-          <div className="top"></div>
-          <div className="center"></div>
-          <div className="bottom"></div>
+          <div className="top">
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/rightArrow.png`}
+              alt="rightArrow"
+              className="left"
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/senDev.jpeg`}
+              alt="rightArrow"
+              className="user"
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/youtube.png`}
+              alt="rightArrow"
+              className="right"
+            />
+          </div>
+          <div className="center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+            impedit sed reprehenderit quo, iure magni molestiae dolor
+          </div>
+          <div className="bottom">
+            <h3>David</h3>
+            <h4>CEO of Cashdo Ltd.</h4>
+          </div>
         </div>
       </div>
     </div>
